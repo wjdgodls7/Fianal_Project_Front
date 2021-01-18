94c9cbfeeb7a159138f507a4aad2b32d8c66fdc6
HAEiNEe <jhi5576@naver.com>
2021-01-18 20:34:33 +0900

    first front

---
+++ src/Hooks/useInput.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+export default (defaultvalue) => {
+    const [value, setvalue] = useState(defaultvalue);
+
+    const onChange = e => {
+        const {
+            target: { value }
+        } = e;
+        setvalue(value);
+    };
+    return { value, onChange };
+};
---
+++ src/Styles/Theme.js
@@ -0,0 +1,20 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+    bgColor: "#FAFAFA",
+    blackColor: "#262626",
+    darkGreyColor: "#999",
+    lightGreyColor: "#c7c7c7",
+    starColor: "#F2CE5A",
+    navyColor: "#364152",
+    brownColor: "#B3795D",
+    boxBorder: "1px solid #e6e6e6",
+    ivoryColor: "#DB937B",
+    borderRadius: "4px",
+    whiteBox: `
+    border:${BOX_BORDER};
+            border-radius:${BORDER_RADIUS};
+            background-color:white;
+            `
+}
